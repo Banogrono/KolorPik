b0f6d10f0ec1789f755efb8798c56fefd7e0a021
Banogrono <48155039+Banogrono@users.noreply.github.com>
2021-01-23 22:55:11 +0100

    Delete KolorPik directory

--- KolorPik/src/main/KolorPik/main/KolorPik/Controller.java
+++
@@ -1,4 +0,0 @@
-package main.KolorPik;
-
-public class Controller {
-}
--- KolorPik/src/main/KolorPik/module-info.java
+++
@@ -1,7 +0,0 @@
-module KolorPik {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens main.KolorPik to javafx.fxml;
-    exports main.KolorPik;
-}
